bca8bfbf9b0b421b50420deeb2df075c894089db
SRINJAYDASGUPTA-Git <dasguptasrinjay2004@gmail.com>
2024-07-27 12:27:41 +0530

    chore: Remove unused test page and add email sanitization utility function

--- kickstart_frontend/lib/utils.ts
+++ kickstart_frontend/lib/utils.ts
@@ -4,3 +4,23 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export function sanitizeEmail(email: string): string {
+  // Replace '@' with '_'
+  let sanitizedEmail = email.replace(/@/g, '_');
+  
+  // Replace '.' with '-'
+  sanitizedEmail = sanitizedEmail.replace(/\./g, '-');
+  
+  return sanitizedEmail;
+}
+
+export function unsanitizeEmail(sanitizedEmail: string): string {
+  // Replace '_' with '@'
+  let unsanitizedEmail = sanitizedEmail.replace(/_/g, '@');
+  
+  // Replace '-' with '.'
+  unsanitizedEmail = unsanitizedEmail.replace(/-/g, '.');
+  
+  return unsanitizedEmail;
+}
