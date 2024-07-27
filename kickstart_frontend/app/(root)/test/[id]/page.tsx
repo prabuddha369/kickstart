d7b3edb26d0fb758306a447cc62b44dac097e344
prabuddha369 <prabuddhapiku@gmail.com>
2024-07-27 19:24:39 +0530

    mobile view

--- kickstart_frontend/app/(root)/test/[id]/page.tsx
+++ kickstart_frontend/app/(root)/test/[id]/page.tsx
@@ -14,6 +14,7 @@ import {
   FormMessage,
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
+import { useRef } from 'react';
 import { Button } from "@/components/ui/button";
 import {
   Popover,
@@ -33,7 +34,7 @@ import Image from "next/image";
 import { useRouter } from "next/navigation";
 import { EmailTemplate } from '@/components/shared/EmailTemplate';
 import { Resend } from 'resend';
-import toast from "react-hot-toast";
+import toast, { Toaster } from "react-hot-toast";
 
 interface PageProps {
   params: {
@@ -59,6 +60,7 @@ const page = ({ params }: PageProps) => {
   const email = params.id;
   const resend = new Resend(process.env.NEXT_PUBLIC_RESEND_API_KEY);
   const router = useRouter();
+  const popoverRef = useRef<any>(null);
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
     defaultValues: {
@@ -74,6 +76,12 @@ const page = ({ params }: PageProps) => {
     yearArray.push(year);
   }
 
+  const closePopover = () => {
+    if (popoverRef.current) {
+      popoverRef.current.click(); // Simulate a click to close the popover
+    }
+  };
+
   async function sendEmail(values: z.infer<typeof formSchema>) {
     try {
       const response = await fetch('/api/send', {
@@ -83,7 +91,7 @@ const page = ({ params }: PageProps) => {
         },
         body: JSON.stringify(values),
       });
-  
+
       const data = await response.json();
       if (response.ok) {
         console.log('Email sent successfully:', data);
@@ -97,198 +105,202 @@ const page = ({ params }: PageProps) => {
 
 
 
-function onSubmit(values: z.infer<typeof formSchema>) {
+  function onSubmit(values: z.infer<typeof formSchema>) {
 
-  addUsersToDB(values)
-    .then(() => {
-      console.log("User added to DB");
-      // sendEmail(values);
-      router.push("/");
-      toast.success('Thanks for joining Kickstart! You are now on our waitlist! Stay tuned for updates and get ready for something great.',
-        {
-          icon: '🎉',
-          style: {
-            borderRadius: '10px',
-            background: '#333',
-            color: '#fff',
-          },
-          duration: 8000,
-          position="bottom-center",
-        }
-      );
+    addUsersToDB(values)
+      .then(() => {
+        console.log("User added to DB");
+        // sendEmail(values);
+        router.push("/");
+        toast.success('Thanks for joining Kickstart! You are now on our waitlist! Stay tuned for updates and get ready for something great.',
+          {
+            icon: '🎉',
+            style: {
+              borderRadius: '10px',
+              background: '#333',
+              color: '#fff',
+            },
+            duration: 8000,
+          }
+        );
 
-    })
-    .catch((error) => {
-      console.error("Error adding user to DB: ", error);
-    });
-}
+      })
+      .catch((error) => {
+        console.error("Error adding user to DB: ", error);
+      });
+  }
 
-return (
-  <div className="bg-[url('/formbg_mob.png')] md:bg-[url('/formbg.png')] w-full h-screen flex items-center flex-col md:flex-row p-3 gap-5">
-    <div className="md:w-1/2 flex items-center justify-center">
-      <Image src={'/logo.svg'} width={275} height={200} alt="Kickstart logo" className="md:w-[500px]" />
+  return (
+    <div className="bg-[url('/formbg_mob.png')] md:bg-[url('/formbg.png')] w-full h-screen flex items-center flex-col md:flex-row p-3 gap-5">
+      <Toaster
+        position="bottom-center"
+        reverseOrder={false}
+      />
+      <div className="md:w-1/2 flex items-center justify-center md:my-0 my-10">
+        <Image src={'/logo.svg'} width={275} height={200} alt="Kickstart logo" className="md:w-[500px]" />
+      </div>
+      <span className="md:hidden block font-bold md:text-4xl text-2xl">Sign up for Waitlist</span>
+      <div className="w-[94%] h-fit py-10 gap-2 md:h-full md:w-1/2 bg-white/20 rounded-xl backdrop-blur-md flex items-center justify-center md:p-5 flex-col">
+        <span className="md:block hidden font-bold md:text-4xl text-2xl mb-6">Sign up for Waitlist</span>
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)} className="w-[90%] space-y-10 md:w-[60%] text-xl">
+            <FormField
+              control={form.control}
+              name="email"
+
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel className="text-xl">Email </FormLabel>
+                  <FormControl>
+                    <Input {...field} className="bg-white text-black" />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="fullName"
+              render={({ field }) => (
+                <FormItem >
+                  <FormLabel className="text-xl">Full Name</FormLabel>
+                  <FormControl>
+                    <Input {...field} placeholder="Enter your full name" className="bg-white text-black" />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="birth_year"
+              render={({ field }) => (
+                <FormItem className="flex flex-col">
+                  <FormLabel className="text-xl">Birth Year</FormLabel>
+                  <Popover>
+                    <PopoverTrigger asChild>
+                      <FormControl>
+                        <Button
+                          variant="outline"
+                          role="combobox"
+                          className={cn(
+                            "w-full justify-between bg-white text-black",
+                            !field.value && "text-black"
+                          )}
+                        >
+                          {field.value
+                            ? yearArray.find((year) => year === field.value)
+                            : "Select type"}
+                        </Button>
+                      </FormControl>
+                    </PopoverTrigger>
+                    <PopoverContent className="w-[200px] p-0 bg-white text-black">
+                      <Command>
+                        <CommandInput
+                          placeholder="Search Year..."
+                          className="h-9"
+                        />
+                        <CommandList>
+                          <CommandEmpty>No Year found.</CommandEmpty>
+                          <CommandGroup>
+                            {yearArray.map((year) => (
+                              <CommandItem
+                                value={year.toString()}
+                                key={year}
+                                onSelect={() => {
+                                  form.setValue("birth_year", year);
+                                }}
+                              >
+                                {year}
+                                <CheckIcon
+                                  className={cn(
+                                    "ml-auto h-4 w-4",
+                                    year === field.value
+                                      ? "opacity-100"
+                                      : "opacity-0"
+                                  )}
+                                />
+                              </CommandItem>
+                            ))}
+                          </CommandGroup>
+                        </CommandList>
+                      </Command>
+                    </PopoverContent>
+                  </Popover>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="type"
+              render={({ field }) => (
+                <FormItem className="flex flex-col">
+                  <FormLabel className="text-xl">Select Your Status</FormLabel>
+                  <Popover>
+                    <PopoverTrigger asChild>
+                      <FormControl>
+                        <Button
+                          variant="outline"
+                          role="combobox"
+                          className={cn(
+                            "w-full justify-between bg-white text-black",
+                            !field.value && "text-[#7a7a7a]"
+                          )}
+                        >
+                          {field.value
+                            ? types.find((type) => type.value === field.value)
+                              ?.label
+                            : "Select type"}
+                        </Button>
+                      </FormControl>
+                    </PopoverTrigger>
+                    <PopoverContent className="w-[200px] p-0  bg-white text-black">
+                      <Command>
+                        <CommandInput
+                          placeholder="Search Type..."
+                          className="h-9"
+                        />
+                        <CommandList>
+                          <CommandEmpty>No type found.</CommandEmpty>
+                          <CommandGroup>
+                            {types.map((type) => (
+                              <CommandItem
+                                value={type.label}
+                                key={type.value}
+                                onSelect={() => {
+                                  form.setValue("type", type.value);
+                                }}
+                              >
+                                {type.label}
+                                <CheckIcon
+                                  className={cn(
+                                    "ml-auto h-4 w-4",
+                                    type.value === field.value
+                                      ? "opacity-100"
+                                      : "opacity-0"
+                                  )}
+                                />
+                              </CommandItem>
+                            ))}
+                          </CommandGroup>
+                        </CommandList>
+                      </Command>
+                    </PopoverContent>
+                  </Popover>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <div className="w-full flex items-center justify-center">
+
+              <Button type="submit" className="rounded-full px-5 text-xl bg-gradient-to-tr from-[#195099] to-[#2A86FF] ">Join Waitlist</Button>
+            </div>
+          </form>
+        </Form>
+      </div>
     </div>
-    <div className="w-full md:w-1/2 bg-white/20 rounded-xl backdrop-blur-md h-full flex items-center justify-center md:p-5 flex-col gap-5 p-3">
-      <span className="font-bold md:text-4xl text-2xl">Sign up for Waitlist</span>
-      <Form {...form}>
-        <form onSubmit={form.handleSubmit(onSubmit)} className="w-[90%] space-y-10 md:w-[60%] text-xl">
-          <FormField
-            control={form.control}
-            name="email"
-
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel className="text-xl">Email </FormLabel>
-                <FormControl>
-                  <Input {...field} className="bg-white text-black" />
-                </FormControl>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-          <FormField
-            control={form.control}
-            name="fullName"
-            render={({ field }) => (
-              <FormItem >
-                <FormLabel className="text-xl">Full Name</FormLabel>
-                <FormControl>
-                  <Input {...field} placeholder="Enter your full name" className="bg-white text-black" />
-                </FormControl>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-          <FormField
-            control={form.control}
-            name="birth_year"
-            render={({ field }) => (
-              <FormItem className="flex flex-col">
-                <FormLabel className="text-xl">Birth Year</FormLabel>
-                <Popover>
-                  <PopoverTrigger asChild>
-                    <FormControl>
-                      <Button
-                        variant="outline"
-                        role="combobox"
-                        className={cn(
-                          "w-[200px] justify-between bg-white text-black",
-                          !field.value && "text-black"
-                        )}
-                      >
-                        {field.value
-                          ? yearArray.find((year) => year === field.value)
-                          : "Select type"}
-                      </Button>
-                    </FormControl>
-                  </PopoverTrigger>
-                  <PopoverContent className="w-[200px] p-0 bg-white text-black">
-                    <Command>
-                      <CommandInput
-                        placeholder="Search Year..."
-                        className="h-9"
-                      />
-                      <CommandList>
-                        <CommandEmpty>No Year found.</CommandEmpty>
-                        <CommandGroup>
-                          {yearArray.map((year) => (
-                            <CommandItem
-                              value={year.toString()}
-                              key={year}
-                              onSelect={() => {
-                                form.setValue("birth_year", year);
-                              }}
-                            >
-                              {year}
-                              <CheckIcon
-                                className={cn(
-                                  "ml-auto h-4 w-4",
-                                  year === field.value
-                                    ? "opacity-100"
-                                    : "opacity-0"
-                                )}
-                              />
-                            </CommandItem>
-                          ))}
-                        </CommandGroup>
-                      </CommandList>
-                    </Command>
-                  </PopoverContent>
-                </Popover>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-          <FormField
-            control={form.control}
-            name="type"
-            render={({ field }) => (
-              <FormItem className="flex flex-col">
-                <FormLabel className="text-xl">Select Your Status</FormLabel>
-                <Popover>
-                  <PopoverTrigger asChild>
-                    <FormControl>
-                      <Button
-                        variant="outline"
-                        role="combobox"
-                        className={cn(
-                          "w-[200px] justify-between bg-white text-black",
-                          !field.value && "text-[#7a7a7a]"
-                        )}
-                      >
-                        {field.value
-                          ? types.find((type) => type.value === field.value)
-                            ?.label
-                          : "Select type"}
-                      </Button>
-                    </FormControl>
-                  </PopoverTrigger>
-                  <PopoverContent className="w-[200px] p-0  bg-white text-black">
-                    <Command>
-                      <CommandInput
-                        placeholder="Search Type..."
-                        className="h-9"
-                      />
-                      <CommandList>
-                        <CommandEmpty>No type found.</CommandEmpty>
-                        <CommandGroup>
-                          {types.map((type) => (
-                            <CommandItem
-                              value={type.label}
-                              key={type.value}
-                              onSelect={() => {
-                                form.setValue("type", type.value);
-                              }}
-                            >
-                              {type.label}
-                              <CheckIcon
-                                className={cn(
-                                  "ml-auto h-4 w-4",
-                                  type.value === field.value
-                                    ? "opacity-100"
-                                    : "opacity-0"
-                                )}
-                              />
-                            </CommandItem>
-                          ))}
-                        </CommandGroup>
-                      </CommandList>
-                    </Command>
-                  </PopoverContent>
-                </Popover>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-          <div className="w-full flex items-center justify-center">
-
-            <Button type="submit" className="rounded-full px-5 text-xl bg-gradient-to-tr from-[#195099] to-[#2A86FF] ">Join Waitlist</Button>
-          </div>
-        </form>
-      </Form>
-    </div>
-  </div>
-);
+  );
 };
 
 export default page;
